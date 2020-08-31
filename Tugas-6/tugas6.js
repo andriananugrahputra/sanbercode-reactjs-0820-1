a5398938882dbbc5316028324c9250c07d4c990a
andriananugrahputra <andriananugrah.putra@gmail.com>
2020-08-31 15:32:46 +0700

    update soal no 1 tugas 6

---
+++ Tugas-6/tugas6.js
@@ -0,0 +1,32 @@
+// SOAL NO 1
+const pi= 3.14;
+
+// Rumus luas lingkaran L = π.r.r
+let luas_lingkaran = (jari_jari) => {
+  return  pi * jari_jari * jari_jari;
+}
+
+// Rumus keliling lingkaran K = 2.π.r
+let keliling_lingkaran = (jari_jari) => {
+  return 2 * pi * jari_jari;
+}
+
+console.log(luas_lingkaran(70));
+console.log(keliling_lingkaran(17));
+// -----------------------------------------
+
+// SOAL NO 2
+
+// -----------------------------------------
+
+// SOAL NO 3
+
+// -----------------------------------------
+
+// SOAL NO 4
+
+// -----------------------------------------
+
+// SOAL NO 5
+
+// -----------------------------------------
