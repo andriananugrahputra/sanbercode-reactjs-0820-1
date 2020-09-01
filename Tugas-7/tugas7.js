53a6dd04ec03f8ef23695294cccfd9d3acb95516
andriananugrahputra <andriananugrah.putra@gmail.com>
2020-09-01 23:34:01 +0700

    update no 2 tugas 7

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -42,17 +42,40 @@ kodok.jump() // "hop hop"
 // --------------------------------
 
 // SOAL NO 2
+class Clock {
+	constructor({template}){
+		this.template = template
+	}
+    render = () => {
+		var date = new Date();
 
-// --------------------------------
+		var hours = date.getHours();
+		if (hours < 10) hours = '0' + hours;
 
-// SOAL NO 3
+		var mins = date.getMinutes();
+		if (mins < 10) mins = '0' + mins;
 
-// --------------------------------
+		var secs = date.getSeconds();
+		if (secs < 10) secs = '0' + secs;
 
-// SOAL NO 4
+		var output = this.template
+			.replace('h', hours)
+			.replace('m', mins)
+			.replace('s', secs);
 
-// --------------------------------
+		console.log(output);
+	}
 
-// SOAL NO 5
+    stop() {
+		clearInterval(this.timer);
+    };
 
+    start() {
+		this.render();
+		this.timer = setInterval(this.render, 1000);
+    };
+}
+
+var clock = new Clock({template: 'h:m:s'});
+clock.start();
 // --------------------------------
